5e2ae326d98eafaca885efff47e367875d8739fd
Noam Moshenberg <noam.mosh@gmail.com>
2024-12-06 18:25:45 +0200

    disable navigation to wallet if not connected to metaMask

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { MouseEvent } from 'react'
 import { NavLink } from 'react-router'
 import { useSelector } from 'react-redux'
 
@@ -7,9 +8,17 @@ import tweedLogoLight from '../../assets/logo-tweed-light.svg'
 import tweedLogoDark from '../../assets/logo-tweed-dark.svg'
 
 import styles from './Style.module.scss'
+import { useCallback } from 'react'
 
 function Header() {
 	const { isDarkMode } = useSelector((state: RootState) => state.theme)
+	const { walletId } = useSelector((state: RootState) => state.auth)
+
+	const handleWalletNav = useCallback((e: MouseEvent) => {
+		if (!walletId) {
+			e.preventDefault()
+		}
+	}, [walletId])
 
 	return (
 		<div className={styles.header}>
@@ -20,7 +29,11 @@ function Header() {
 				/>
 			</NavLink>
 
-			<NavLink to="/wallet">
+			<NavLink
+				to="/wallet"
+				onClick={handleWalletNav}
+				className={!walletId ? styles.disabled : undefined}
+			>
 				Wallet
 			</NavLink>
 
